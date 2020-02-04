a47bcf91d0bfcd7f245b5a2ae1fd6fc24d960555
Crystal Ly <crystal.m.ly@gmail.com>
2020-02-04 13:42:20 -0600

    removed 2 html files, added UIKit as CSS framework

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,28 +4,14 @@ var path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-	app.get("/", function(req, res) {
+	app.get("/", function (req, res) {
 		// If the user already has an account send them to the members page
 		if (req.user) {
 			res.redirect("/members");
 		}
 		res.sendFile(path.join(__dirname, "../public/signup.html"));
-	});
-
-	app.get("/login", function(req, res) {
-		// If the user already has an account send them to the members page
-		if (req.user) {
-			res.redirect("/members");
-		}
-		res.sendFile(path.join(__dirname, "../public/login.html"));
-	});
-
-	// Here we've add our isAuthenticated middleware to this route.
-	// If a user who is not logged in tries to access this route they will be redirected to the signup page
-	app.get("/members", isAuthenticated, function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/members.html"));
 	});
 
 };
