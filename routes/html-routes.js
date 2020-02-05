a9457194ed05c61801f79c49c81b08a7a2631f09
Brianna McCray <brianna.mccray@outlook.com>
2020-02-05 10:18:26 -0600

    StufF

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,9 +9,9 @@ module.exports = function (app) {
 	app.get("/", function (req, res) {
 		// If the user already has an account send them to the members page
 		if (req.user) {
-			res.redirect("/members");
+			res.redirect("../public/signup.html"); 
 		}
-		res.sendFile(path.join(__dirname, "../public/signup.html"));
+		// res.sendFile(path.join(__dirname, "../public/signup.html"));
 	});
 
 };
