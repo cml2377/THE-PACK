eeb3978d66de22043a683b6764cd431311674aab
Brianna McCray <brianna.mccray@outlook.com>
2020-02-05 12:50:18 -0600

    Readding Signup Page

--- public/js/signup.js
+++ public/js/signup.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
 			email: email,
 			password: password
 		})
-			.then(function () {
+			.then(function (data) {
 				window.location.replace("/members");
 				// If there's an error, handle it by throwing up a bootstrap alert
 			})
@@ -39,39 +39,4 @@ $(document).ready(function () {
 		$("#alert .msg").text(err.responseJSON);
 		$("#alert").fadeIn(500);
 	}
-
-	// Function that toggles background image of application.
-	function toggleBackgroundImage() {
-		$(".slider").click(function () {
-			var imageURL = "../img/nathan-trampe-cyIERGMF_1U-unsplash.jpg";
-			$(".backgroundImg").css("background-image", "url(" + imageURL + ")");
-		});
-	}
-	toggleBackgroundImage();
 });
- 
-// Get the modal
-var modal = document.getElementById("modal-left");
-
-// Get the button that opens the modal
-var btn = document.getElementById("btn-left");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-	modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-	modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
-};
