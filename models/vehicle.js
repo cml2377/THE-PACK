324771d6bcf720bd30225c5a020d3dfb8925705b
Harrison Thomas <hthomas146@utexas.edu>
2020-02-05 09:59:55 -0600

    Added the models and sql database

---
+++ models/vehicle.js
@@ -0,0 +1,9 @@
+module.exports = function (sequelize, DataTypes) {
+    var Vehicle = sequelize.define("Vehicle", {
+        category: DataTypes.STRING,
+        make: DataTypes.STRING,
+        model: DataTypes.STRING,
+        year: DataTypes.INTEGER
+    });
+    return Vehicle;
+};
