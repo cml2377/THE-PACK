d082d52e7f29b86a13fcf6c3ad30b4973d44461b
Brianna McCray <brianna.mccray@outlook.com>
2020-02-05 21:14:49 -0600

    Working on EventBrite API

--- public/js/members.js
+++ public/js/members.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+
+	const eventBriteBtnEl = $("#eventBriteBtn");
+	const eventFrameEl = $("#eventBriteFrame");
+	const eventBriteDivEl = $("#eventBriteDiv");
 	// This file just does a GET request to figure out which user is logged in
 	// and updates the HTML on the page
 	$.get("/api/user_data").then(function (data) {
@@ -8,4 +12,12 @@ $(document).ready(function () {
 	$(".slider").click(function () {
 		$("#backgroundImageInDiv").toggleClass("active");
 	});
+
+	$(eventBriteBtnEl).on("click", function() {
+		console.log("Creating Event Frame....hopefully");
+		eventFrameEl.attr("src", "https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=LV7ITGSKLPHYPZ5JE7&redirect_uri=https://www.eventbrite.com/d/tx--austin/motorcycle-shows/");
+		eventBriteDivEl.append(eventFrameEl);
+	});
+
+
 });
