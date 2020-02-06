ac83023a6a07c1e055237531ba3a023ccb70e3c9
Harrison Thomas <hthomas146@utexas.edu>
2020-02-06 13:23:20 -0600

    API front-end request now works mostly

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,11 +1,15 @@
 module.exports = function (sequelize, DataTypes) {
-	var Car = sequelize.define("Car", {
+	var Vehicle = sequelize.define("Vehicle", {
+		category: DataTypes.STRING,
 		make: DataTypes.STRING,
 		model: DataTypes.STRING,
 		series: DataTypes.STRING,
 		plantCountry: DataTypes.STRING,
 		year: DataTypes.INTEGER
-
 	});
-	return Car;
+
+	Vehicle.associate = function (models) {
+		Vehicle.hasMany(models.User)
+	}
+	return Vehicle;
 };
