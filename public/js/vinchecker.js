7e501681b2b16344d3898b672bf90044f5302424
Harrison Thomas <hthomas146@utexas.edu>
2020-02-05 21:03:06 -0600

    Linter fixed issues

--- public/js/vinchecker.js
+++ public/js/vinchecker.js
@@ -9,32 +9,32 @@ let plantCountry;
 let series;
 
 inquirer
-    .prompt([
-        {
-            name: "vehicleVin",
-            type: "prompt",
-            message: "Please enter the vehicle's VIN"
-        }
-    ]).then(function (data) {
-        vinSearch(data);
-    })
+	.prompt([
+		{
+			name: "vehicleVin",
+			type: "prompt",
+			message: "Please enter the vehicle's VIN"
+		}
+	]).then(function (data) {
+		vinSearch(data);
+	});
 
 function vinSearch(data) {
-    axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${data.vehicleVin}?format=json`)
-        .then(function (response) {
-            make = response.data.Results[6].Value;
-            model = response.data.Results[8].Value;
-            year = response.data.Results[9].Value;
-            plantCountry = response.data.Results[14].Value;
-            series = response.data.Results[18].Variable;
-            // console.log(response.data);
-            console.log(make);
-            console.log(model);
-            console.log(year);
-            console.log(plantCountry);
-            console.log(series);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+	axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${data.vehicleVin}?format=json`)
+		.then(function (response) {
+			make = response.data.Results[6].Value;
+			model = response.data.Results[8].Value;
+			year = response.data.Results[9].Value;
+			plantCountry = response.data.Results[14].Value;
+			series = response.data.Results[18].Variable;
+			// console.log(response.data);
+			console.log(make);
+			console.log(model);
+			console.log(year);
+			console.log(plantCountry);
+			console.log(series);
+		})
+		.catch(error => {
+			console.log(error);
+		});
 }
