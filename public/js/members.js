b4627f7b710fc13e829b2545f3e1d2c584bc6bec
Crystal Ly <crystal.m.ly@gmail.com>
2020-02-07 13:52:44 -0600

    removed axios call from members.js

--- public/js/members.js
+++ public/js/members.js
@@ -51,32 +51,4 @@ $(document).ready(function () {
 
 	//==========================================================
 
-	// This prompts the user for their location. This is for EVENTS.
-	var options = {
-		enableHighAccuracy: true,
-		timeout: 5000,
-		maximumAge: 0
-	};
-
-	function success(pos) {
-		var crd = pos.coords;
-		//if user provides geolocation: 
-		console.log("Your current position is:");
-		console.log(`Latitude : ${crd.latitude}`);
-		console.log(`Longitude: ${crd.longitude}`);
-		console.log(`More or less ${crd.accuracy} meters.`);
-		axios.post("/", { // We call for the information from the back-end using the front-end"s provided location.
-			data: { latitude: crd.latitude, longitude: crd.longitude }
-		}).then(function (response) {
-			//Log the resulting object
-			console.log(response);
-		});
-	}
-
-	function error(err) {
-		console.warn(`ERROR(${err.code}): ${err.message}`);
-		//if user denies geolocation, run Austin as default.
-	}
-	navigator.geolocation.getCurrentPosition(success, error, options);
-
 });
