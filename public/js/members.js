0161f11c971ec9be5690012d576ab70fdb8f5424
Crystal Ly <crystal.m.ly@gmail.com>
2020-02-07 13:44:41 -0600

    dealing with vehicle definitions

--- public/js/members.js
+++ public/js/members.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 	//          The main page! This is the homepage the user is
 	//          directed to after logging in. Holds markers. 
 	//===========================================================================
-	const axios = require("axios");
 
 	// This file just does a GET request to figure out which user is logged in
 	// and updates the HTML on the page
@@ -26,14 +25,29 @@ $(document).ready(function () {
 	dropdown.append("<option>Define parts...</option>");
 	dropdown.prop("selectedIndex", 0);
 
-	const url = "https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablelist";
-
 	// Populate dropdown with list of part names
-	$.getJSON(url, function (data) {
-		$.each(data, function (key, entry) {
-			dropdown.append($("<option></option>").attr("value", entry.abbreviation).text(entry.name));
+	$("select[data-source]").each(function () {
+		var $select = $(this);
+
+		$select.append("<option></option>");
+
+		$.ajax({
+			url: $select.attr("data-source"),
+		}).then(function (options) {
+			options.map(function (option) {
+				var $option = $("<option>");
+
+				$option
+					.val(option[$select.attr("data-valueKey")])
+					.text(option[$select.attr("data-displayKey")]);
+
+				$select.append($option);
+			});
 		});
 	});
+
+	// When the user picks a vehicle variable, the definition of the variable is displayed in the div variableResults
+
 
 	//==========================================================
 
