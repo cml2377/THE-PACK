c1c0ffad690d1419b55a4f8a5b7c45fd602f9431
Crystal Ly <crystal.m.ly@gmail.com>
2020-02-07 12:53:19 -0600

    adding vehicle variables

--- public/js/members.js
+++ public/js/members.js
@@ -16,6 +16,27 @@ $(document).ready(function () {
 		$("#backgroundImageInDiv").toggleClass("active");
 	});
 
+	// =========================================================
+
+	// This populates a dropdown with vehicle variables in the modal.
+	let dropdown = $("#vehicleVariables");
+
+	dropdown.empty();
+
+	dropdown.append("<option>Define parts...</option>");
+	dropdown.prop("selectedIndex", 0);
+
+	const url = "https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablelist";
+
+	// Populate dropdown with list of part names
+	$.getJSON(url, function (data) {
+		$.each(data, function (key, entry) {
+			dropdown.append($("<option></option>").attr("value", entry.abbreviation).text(entry.name));
+		});
+	});
+
+	//==========================================================
+
 	// This prompts the user for their location. This is for EVENTS.
 	var options = {
 		enableHighAccuracy: true,
@@ -30,7 +51,7 @@ $(document).ready(function () {
 		console.log(`Latitude : ${crd.latitude}`);
 		console.log(`Longitude: ${crd.longitude}`);
 		console.log(`More or less ${crd.accuracy} meters.`);
-		axios.post("/", { // We call for the information from the back-end using the front-end's provided location.
+		axios.post("/", { // We call for the information from the back-end using the front-end"s provided location.
 			data: { latitude: crd.latitude, longitude: crd.longitude }
 		}).then(function (response) {
 			//Log the resulting object
@@ -43,6 +64,5 @@ $(document).ready(function () {
 		//if user denies geolocation, run Austin as default.
 	}
 	navigator.geolocation.getCurrentPosition(success, error, options);
+
 });
-
-
