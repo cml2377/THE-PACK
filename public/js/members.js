6935bf26677a91b72016aff40c6bb01def7a5748
Crystal Ly <crystal.m.ly@gmail.com>
2020-02-07 11:32:35 -0600

    added geolocation

--- public/js/members.js
+++ public/js/members.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+	//===========================================================================
+	//          The main page! This is the homepage the user is
+	//          directed to after logging in. Holds markers. 
+	//===========================================================================
+	const axios = require("axios");
 
 	// This file just does a GET request to figure out which user is logged in
 	// and updates the HTML on the page
@@ -6,7 +11,38 @@ $(document).ready(function () {
 		$(".member-name").text(data.email);
 	});
 
+	// This slider toggles the background image from a motorcycle to a car.
 	$(".slider").click(function () {
 		$("#backgroundImageInDiv").toggleClass("active");
 	});
+
+	// This prompts the user for their location. This is for EVENTS.
+	var options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0
+	};
+
+	function success(pos) {
+		var crd = pos.coords;
+		//if user provides geolocation: 
+		console.log("Your current position is:");
+		console.log(`Latitude : ${crd.latitude}`);
+		console.log(`Longitude: ${crd.longitude}`);
+		console.log(`More or less ${crd.accuracy} meters.`);
+		axios.post("/", { // We call for the information from the back-end using the front-end's provided location.
+			data: { latitude: crd.latitude, longitude: crd.longitude }
+		}).then(function (response) {
+			//Log the resulting object
+			console.log(response);
+		});
+	}
+
+	function error(err) {
+		console.warn(`ERROR(${err.code}): ${err.message}`);
+		//if user denies geolocation, run Austin as default.
+	}
+	navigator.geolocation.getCurrentPosition(success, error, options);
 });
+
+
