d9f87d5fe6212fd0d86a3512f2178d4758e3ef59
Brianna McCray <brianna.mccray@outlook.com>
2020-02-07 11:17:28 -0600

    THIS IS MY LAST RESORT

---
+++ public/js/vehicle.js
@@ -0,0 +1,21 @@
+const googleBtnEl = $("#googleBtn");
+const googleDivEl = $("#googleDiv");
+
+
+$(googleBtnEl).on("click", function () {
+  console.log("GOOOOOOOOOGLE");
+
+  $.ajax("/api/events/cars").done(function (response) {
+    console.log(response);
+    for(var i = 0; i < 5; i++) {
+      const places = response[i];
+      // console.log(places.name);
+      const ptagEl = $("<p>");
+      ptagEl.text(places.name);
+      googleDivEl.append(ptagEl);
+      console.log(places.photos)
+    }
+  });
+});
+
+
