c6dc580a6d703118ae1dbbf8106691ccfbc038d2
Brianna McCray <brianna.mccray@outlook.com>
2020-02-07 09:21:20 -0600

    PLease work

--- public/js/googlePlaces.js
+++ public/js/googlePlaces.js
@@ -2,26 +2,37 @@ const googleBtnEl = $("#googleBtn");
 const googleDivEl = $("#googleDiv");
 
 
-$(googleBtnEl).on("click", function(){
+$(googleBtnEl).on("click", function () {
   console.log("GOOOOOOOOOGLE");
-  var settings = {
-    "url": "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=car+events&keyAIzaSyDfl4ckbrHuuNG0IdouW6Y70hrSxD1CMJE",
+  const settings = {
+    "url": "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=car+events&key=AIzaSyDfl4ckbrHuuNG0IdouW6Y70hrSxD1CMJE",
     "method": "GET",
     "timeout": 0,
   };
-  
+
   $.ajax(settings).done(function (response) {
     console.log(response.results);
-    for(var i = 0; i < response.results.length; i++) {
+    for (var i = 0; i < response.results.length; i++) {
       const places = response.results[i];
-      
-      const nameEl = $("<p>");          
-      console.log(places.name);     
-      nameEl.text(places.name);      
-      googleDivEl.append(nameEl);           
+
+      const nameEl = $("<p>");
+      console.log(places.name);
+      nameEl.text(places.name);
+      googleDivEl.append(nameEl);
+
+      const queryInput = places.name.replace(" ", "%20");
+      console.log(queryInput);
+      //take the name and manipulate into another ajax call to get the url
+      const query = {
+        "url": `https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${queryInput}&inputtype=textquery&fields=url&key=AIzaSyDfl4ckbrHuuNG0IdouW6Y70hrSxD1CMJE`,
+        "method": "GET",
+        "timeout": 0,
+      };
+      console.log(query);
+
     }
-    
+
   });
-  
+
 });
 
