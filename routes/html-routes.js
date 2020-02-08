d122bcad2d6fba11191bbca7815014cd54e7e11b
Brianna McCray <brianna.mccray@outlook.com>
2020-02-07 21:17:38 -0600

    Commit me to psych ward plzzz

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
-console.log()
+console.log();
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
