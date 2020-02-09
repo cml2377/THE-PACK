fbebe4c4847c13750990c8775e4394c031ca70a7
Crystal Ly <crystal.m.ly@gmail.com>
2020-02-09 12:38:30 -0600

    changing axios source link to see if it will pass Travis CI

--- public/js/nhtsa.js
+++ public/js/nhtsa.js
@@ -1,211 +1,26 @@
-// import Axios from "axios";
+$(document).ready(function () {
 
-// /**
-//  * pulls information from the form and build the query URL
-//  * @returns {string} URL for NYT API based on form inputs
-//  */
-// function buildQueryURL() {
-//     // queryURL is the url we"ll use to query the API
-//     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+    $(".recallSearch-btn").click(function () {
+        // var data = $("#userYear").val();
+        // console.log(data);
+        var make = $("#userMake").val();
+        var model = $("#userModel").val();
+        var year = $("#userYear").val();
+        recallSearch(make, model, year);
+    });
 
-//     // Begin building an object to contain our API call"s query parameters
-//     // Set the API key
-//     var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
-
-//     // Grab text the user typed into the search input, add to the queryParams object
-//     queryParams.q = $("#search-term")
-//       .val()
-//       .trim();
-
-//     // If the user provides a startYear, include it in the queryParams object
-//     var startYear = $("#start-year")
-//       .val()
-//       .trim();
-
-//     if (parseInt(startYear)) {
-//       queryParams.begin_date = startYear + "0101";
-//     }
-
-//     // If the user provides an endYear, include it in the queryParams object
-//     var endYear = $("#end-year")
-//       .val()
-//       .trim();
-
-//     if (parseInt(endYear)) {
-//       queryParams.end_date = endYear + "0101";
-//     }
-
-//     // Logging the URL so we have access to it for troubleshooting
-//     console.log("---------------\nURL: " + queryURL + "\n---------------");
-//     console.log(queryURL + $.param(queryParams));
-//     return queryURL + $.param(queryParams);
-//   }
-
-//   /**
-//    * takes API data (JSON/object) and turns it into elements on the page
-//    * @param {object} NYTData - object containing NYT API data
-//    */
-//   function updatePage(NYTData) {
-//     // Get from the form the number of results to display
-//     // API doesn"t have a "limit" parameter, so we have to do this ourselves
-//     var numArticles = $("#article-count").val();
-
-//     // Log the NYTData to console, where it will show up as an object
-//     console.log(NYTData);
-//     console.log("------------------------------------");
-
-//     // Loop through and build elements for the defined number of articles
-//     for (var i = 0; i < numArticles; i++) {
-//       // Get specific article info for current index
-//       var article = NYTData.response.docs[i];
-
-//       // Increase the articleCount (track article # - starting at 1)
-//       var articleCount = i + 1;
-
-//       // Create the  list group to contain the articles and add the article content for each
-//       var $articleList = $("<ul>");
-//       $articleList.addClass("list-group");
-
-//       // Add the newly created element to the DOM
-//       $("#article-section").append($articleList);
-
-//       // If the article has a headline, log and append to $articleList
-//       var headline = article.headline;
-//       var $articleListItem = $("<li class="list-group-item articleHeadline">");
-
-//       if (headline && headline.main) {
-//         console.log(headline.main);
-//         $articleListItem.append(
-//           "<span class="label label-primary">" +
-//             articleCount +
-//             "</span>" +
-//             "<strong> " +
-//             headline.main +
-//             "</strong>"
-//         );
-//       }
-
-//       // If the article has a byline, log and append to $articleList
-//       var byline = article.byline;
-
-//       if (byline && byline.original) {
-//         console.log(byline.original);
-//         $articleListItem.append("<h5>" + byline.original + "</h5>");
-//       }
-
-//       // Log section, and append to document if exists
-//       var section = article.section_name;
-//       console.log(article.section_name);
-//       if (section) {
-//         $articleListItem.append("<h5>Section: " + section + "</h5>");
-//       }
-
-//       // Log published date, and append to document if exists
-//       var pubDate = article.pub_date;
-//       console.log(article.pub_date);
-//       if (pubDate) {
-//         $articleListItem.append("<h5>" + article.pub_date + "</h5>");
-//       }
-
-//       // Append and log url
-//       $articleListItem.append("<a href="" + article.web_url + "">" + article.web_url + "</a>");
-//       console.log(article.web_url);
-
-//       // Append the article
-//       $articleList.append($articleListItem);
-//     }
-//   }
-
-//   // Function to empty out the articles
-//   function clear() {
-//     $("#article-section").empty();
-//   }
-
-//   // CLICK HANDLERS
-//   // ==========================================================
-
-//   // .on("click") function associated with the Search Button
-//   $("#run-search").on("click", function(event) {
-//     // This line allows us to take advantage of the HTML "submit" property
-//     // This way we can hit enter on the keyboard and it registers the search
-//     // (in addition to clicks). Prevents the page from reloading on form submit.
-//     event.preventDefault();
-
-//     // Empty the region associated with the articles
-//     clear();
-
-//     // Build the query URL for the ajax request to the NYT API
-//     var queryURL = buildQueryURL();
-
-//     // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-//     // The data then gets passed as an argument to the updatePage function
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(updatePage);
-//   });
-
-//   //  .on("click") function associated with the clear button
-//   $("#clear-all").on("click", clear);
+    function recallSearch(make, model, year) {
+        // axios.get("https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/2017/make/infiniti/model/q50?format=json")    
+        axios.get(`https://cors-ut-bootcamp.herokuapp.com/https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/${year}/make/${make}/model/${model}?format=json`)
+            // axios.get("https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/2017/make/infiniti/model/q50?format=json")
 
 
+            .then(function (response) {
 
+                for (let index = 0; index < response.data.Results.length; index++) {
+                    const recall = response.data.Results[index];
 
-
-// const ajax = require("ajax");
-
-// $.ajax({
-//     URL: "https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/2017/make/infiniti/model/q50?format=json",
-//     method: "GET"
-// }).then(function (res) {
-//     console.log(res, "You"ve made a response!");
-// })
-
-
-// const ajax = require("ajax");
-
-$(".recallSearch-btn").click(function () {
-    // var data = $("#userYear").val();
-    // console.log(data);
-    var make = $("#userMake").val();
-    var model = $("#userModel").val();
-    var year = $("#userYear").val();
-    recallSearch(make, model, year);
-});
-
-// let ({"year"}) = userYear;//get this from the front end 
-// // select1 = ({ year });
-// let {"make"} = userMake
-// // // select2 = ({ make });
-// let {model} = userModel
-// select3 = ({ model });
-// var response = "https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/2017/make/infiniti/model/q50?format=json";
-
-
-function recallSearch(make, model, year) {
-    // axios.get("https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/2017/make/infiniti/model/q50?format=json")    
-    axios.get(`https://cors-ut-bootcamp.herokuapp.com/https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/${year}/make/${make}/model/${model}?format=json`)
-        // axios.get("https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/2017/make/infiniti/model/q50?format=json")
-
-
-        .then(function (response) {
-            // var data = {
-            //     modelYear: response.data.Results[0].ModelYear,
-            //     make: response.data.Results[0].Make,
-            //     model: response.data.Results[0].Model,
-            //     component: response.data.Results[0].Component,
-            //     summary: response.data.Results[0].Summary,
-            //     remedy: response.data.Results[0].Remedy
-
-            // }
-            // var recall = response.results;
-            //         // summary = response.data.Results[5].Value;
-            //     //     // consequence = response.data.Results[6].Value;
-            //     //     // remedy = response.data.Results[7].Value;
-            for (let index = 0; index < response.data.Results.length; index++) {
-                const recall = response.data.Results[index];
-
-                let batman = `
+                    let batman = `
                 <div>
                 <p class = "recallInfo">${recall.ModelYear}</p>
                 <p class = "recallInfo">${recall.Make}</p>
@@ -215,27 +30,8 @@ function recallSearch(make, model, year) {
                 <p class = "recallInfo">${recall.Remedy}</p>
                 </div>`;
 
-                $("#recallResults").append(batman);
-            }
-
-
-            // });
-        });
-}
-
-
-
-// $.ajax({
-//     url: `https://cors-ut-bootcamp.herokuapp.com/https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/${year}/make/${make}/model/${model}?format=json`,
-//     type: "GET",
-//     dataType: "json",
-//     success: function (result) {
-//         console.log(result);
-//     },
-//     error: function (xhr, ajaxOptions, thrownError) {
-//         console.log(xhr.status);
-//         console.log(thrownError);
-//     }
-// });
-
-// .MAP
+                    $("#recallResults").append(batman);
+                }
+            });
+    }
+});
