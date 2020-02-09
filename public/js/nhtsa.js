66a1abbf34dd91f3cbb5a48bec700bd22e2d45bb
Crystal Ly <crystal.m.ly@gmail.com>
2020-02-09 12:54:51 -0600

    changed axios call to ajax

--- public/js/nhtsa.js
+++ public/js/nhtsa.js
@@ -10,12 +10,10 @@ $(document).ready(function () {
     });
 
     function recallSearch(make, model, year) {
-        // axios.get("https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/2017/make/infiniti/model/q50?format=json")    
-        axios.get(`https://cors-ut-bootcamp.herokuapp.com/https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/${year}/make/${make}/model/${model}?format=json`)
-            // axios.get("https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/2017/make/infiniti/model/q50?format=json")
 
+        $.ajax(`https://cors-ut-bootcamp.herokuapp.com/https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/${year}/make/${make}/model/${model}?format=json`)
 
-            .then(function (response) {
+            .done(function (response) {
 
                 for (let index = 0; index < response.data.Results.length; index++) {
                     const recall = response.data.Results[index];
