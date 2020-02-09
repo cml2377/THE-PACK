45def552a956bfb07077b832d479511702cf3ab2
Brianna McCray <brianna.mccray@outlook.com>
2020-02-09 13:06:40 -0600

    Uncommented Crystal's code, sorry Crystal!

--- public/js/members.js
+++ public/js/members.js
@@ -11,55 +11,55 @@ $(document).ready(function () {
 	});
 
 	// This slider toggles the background image from a motorcycle to a car.
-	// $(".slider").click(function () {
-	// 	$("#backgroundImageInDiv").toggleClass("active");
-	// });
+	$(".slider").click(function () {
+		$("#backgroundImageInDiv").toggleClass("active");
+	});
 
-	// // =========================================================
+	// =========================================================
 
-	// // This populates a dropdown with vehicle variables in the modal.
-	// let dropdown = $("#vehicleVariables");
+	// This populates a dropdown with vehicle variables in the modal.
+	let dropdown = $("#vehicleVariables");
 
-	// dropdown.empty();
+	dropdown.empty();
 
-	// dropdown.append("<option>Define parts...</option>");
+	dropdown.append("<option>Define parts...</option>");
 
-	// // Populate dropdown with list of part names
+	// Populate dropdown with list of part names
 
-	// let userSelectedDesc = {};
+	let userSelectedDesc = {};
 
-	// $.ajax({
-	// 	url: "https://cors-ut-bootcamp.herokuapp.com/https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablelist?format=json",
-	// 	method: "GET"
-	// }).then(function (response) {
-	// 	for (var i = 0; i < response.Results.length; i++) {
-	// 		const partName = response.Results[i].Name;
-	// 		const ID = response.Results[i].ID;
-	// 		$("#vehicleVariables").append("<option value=" + ID + ">" + partName + "</option>");
-	// 		userSelectedDesc[ID] = response.Results[i].Description;
-	// 	}
-	// });
+	$.ajax({
+		url: "https://cors-ut-bootcamp.herokuapp.com/https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablelist?format=json",
+		method: "GET"
+	}).then(function (response) {
+		for (var i = 0; i < response.Results.length; i++) {
+			const partName = response.Results[i].Name;
+			const ID = response.Results[i].ID;
+			$("#vehicleVariables").append("<option value=" + ID + ">" + partName + "</option>");
+			userSelectedDesc[ID] = response.Results[i].Description;
+		}
+	});
 
-	// dropdown.change(function () {
-	// 	// When the user picks a name, the explanation of the variable is displayed in the div variableResults. This is based on each option's value(ID). 
-	// 	console.log(this.value);
-	// 	var value = this.value;
-	// 	$("#variableResults").empty().append(userSelectedDesc[value]);
+	dropdown.change(function () {
+		// When the user picks a name, the explanation of the variable is displayed in the div variableResults. This is based on each option's value(ID). 
+		console.log(this.value);
+		var value = this.value;
+		$("#variableResults").empty().append(userSelectedDesc[value]);
 
-	// });
+	});
 
 
-	//========================================================== 
-	// 				Media Queries
-	// var mql = window.matchMedia("screen and (maxwidth: 768px)");
-	// mql.addListener(function (viewSize) {
-	// 	if (viewSize.matches) {
-	// 		UIkit.tooltip("title").show();
-	// 	}
-	// 	else {
-	// 		console.log("Not on mobile");
-	// 	}
-	// });
+	// ========================================================== 
+					// Media Queries
+	var mql = window.matchMedia("screen and (maxwidth: 768px)");
+	mql.addListener(function (viewSize) {
+		if (viewSize.matches) {
+			UIkit.tooltip("title").show();
+		}
+		else {
+			console.log("Not on mobile");
+		}
+	});
 
 	//==========================================================
 
